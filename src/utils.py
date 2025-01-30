a25e10834286ad7a78abf12abee7ac61e9856f14
amagrabi <amadeus.magrabi@gmail.com>
2025-01-30 12:25:45 +0100

    Fix src path resolution in

--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,12 @@ import sys
 
 
 def _resource_path(relative_path):
-    """Get absolute path to resource, works for dev and for PyInstaller"""
+    """Get absolute path to resource that works for dev and PyInstaller"""
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
+        base_path = os.path.join(base_path, "src")
 
     return os.path.join(base_path, relative_path)
